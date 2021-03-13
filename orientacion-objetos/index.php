7bdd68dc04f4f7164f33e72f185edb97d9948cd5
fullstack82 <fullstack8210@gmail.com>
2021-03-13 23:31:02 +0100

    POO

---
+++ orientacion-objetos/index.php
@@ -0,0 +1,36 @@
+<?php
+// Programacion orientada a objetos en PHP (POO)
+
+// Definir una clase (molde para crear más objetos de tipo coche con caracteristicas similares o parecidas);
+// Las clases se definen con Mayusculas;
+class Coche {
+
+    // Atributos o propiedades (variables);
+
+    public $color = "lila";
+    public $marca = "alpha";
+    public $modelo = "sport";
+    public $velocidad = 250;
+    public $caballos = 400;
+    public $plazas = 5;
+
+    // Metodos, son acciones que hace el objeto (antes funciones)
+    public function getColor(){
+        // Busca en esta clase la propiedad X
+        return $this->color;
+    }
+
+    public function setColor($color){
+        $this->color = $color;
+    }
+
+    public function acelerar(){
+        $this->velocidad++;
+    }
+
+    public function frenar(){
+        $this->velocidad--;
+    }
+
+    
+}
