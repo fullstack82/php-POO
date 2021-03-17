1350906b0eb59876c0c63ea24f27b3c583baebda
fullstack82 <fullstack8210@gmail.com>
2021-03-17 22:47:04 +0100

    propiedades y metodos estaticos

---
+++ orientacion-objetos/estaticas/configuracion.php
@@ -0,0 +1,39 @@
+<?php
+/*Propiedades y metodos estaticos*/
+class Configuracion
+{
+
+    public static $color;
+    public static $newsletter;
+    public static $entorno;
+
+
+    public static function getColor()
+    {
+        return self::$color;
+    }
+
+    public static function getNewsletter()
+    {
+        return self::$newsletter;
+    }
+
+    public static function getEntorno()
+    {
+        return self::$entorno;
+    }
+
+    public static function setColor($color){
+        self::$color = $color;
+    }
+
+    public static function setNewletter($newsletter){
+        self::$newsletter = $newsletter;
+    }
+
+    public static function setEntorno($entorno){
+        self::$entorno = $entorno;
+    }
+
+
+}
